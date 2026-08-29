8de8d34b38804446615bbc7b34b5cdc2b6fb4de6
Hassan-Ahuzali <hassan407@proton.me>
2026-08-29 23:54:24 +0300

    Improve docstrings and formatting in async tasks
    Update docstrings to better adhere to style guidelines and improve code
    readability across the async comprehension module. Add a README file for
    project documentation.

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Module for async generator"""
+"""This module defines an asynchronous generator."""
 
 import asyncio
 import random
@@ -7,9 +7,7 @@ from typing import Generator
 
 
 async def async_generator() -> Generator[float, None, None]:
-    """Module to asynchronously wait 1 second then yield a number between
-    0 and 10.
-    """
-    for i in range(10):
+    """Yield ten random floating-point numbers asynchronously."""
+    for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
