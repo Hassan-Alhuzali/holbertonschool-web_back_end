0e24533bbba8c5738d0150038b98afddce087346
Hassan-Ahuzali <hassan407@proton.me>
2026-09-10 18:05:45 +0300

    Refactor index_range

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python3
 
 '''
-index_range - returns a tuple of start and end indexes for a given page and page size
+returns a tuple of start and end indexes for a given page and page size
 '''
 def index_range(page, page_size):
     '''
     Calculates the start and end indexes for a given page and page size
-    
     '''
     start_index = (page - 1) * page_size
-    end_index = start_index + page_size
-    print(f"page: {start_index}, number of items: {end_index}")
-    return (start_index, end_index)
+    end_index = page * page_size
+    return tuple([start_index, end_index])
